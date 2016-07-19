baa5ac21e79a4c991a879dbf8796811f57e89d81
davidnoahball <davidnoahball@gmail.com>
2016-07-19 14:06:50 -0700

    Started Printer#print_page

--- lib/apod/cli.rb
+++ lib/apod/cli.rb
@@ -1,6 +1,9 @@
 require_relative "../apod/scraper.rb"
 require_relative "../apod/printer.rb"
+require "colorize"
 
 scrape = Scraper.new
 print = Printer.new
-puts print.print_link(scrape.data[0])
+print.print_link(scrape.data[1000], "[1]".colorize(:red))
+print.print_link(scrape.data[508], "[2]".colorize(:red))
+print.print_page({expl: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat lorem metus, vel ornare libero suscipit vel. Fusce facilisis mattis sem, a mattis sem aliquam vitae. Donec pellentesque mattis pellentesque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce posuere mauris sit amet augue consectetur, id malesuada quam consequat. Aliquam id risus et lacus rutrum sagittis et vitae mi. Nulla eget enim massa. In malesuada iaculis purus nec aliquet. Vestibulum maximus est in dictum vulputate. Sed feugiat ipsum ac lorem facilisis, at efficitur eros imperdiet. Aenean justo ante, eleifend sit amet iaculis ut, luctus eu magna. Suspendisse ut diam vel tortor hendrerit consequat. Donec magna neque, sodales vitae diam at, pulvinar auctor odio. Sed eget ipsum aliquet nulla placerat volutpat. In efficitur metus vitae eros luctus congue."})
--- lib/apod/printer.rb
+++ lib/apod/printer.rb
@@ -1,10 +1,32 @@
 require "colorize"
 
 class Printer
-  def print_link(link_hash)
-    "The astronomy picture of the day on " + link_hash[:date].colorize(:green) + " was " + link_hash[:name].colorize(:green) + "."
+  def print_link(link_hash, start="")
+    puts start + "The astronomy picture of the day on " + link_hash[:date].colorize(:green) + " was " + link_hash[:name].colorize(:green) + "."
   end
-  def print_page
-
+  def print_page(page_hash)
+    words_arr = page_hash[:expl].split(" ")
+    line = ""
+    lines_arr = []
+    max_line_length = 0
+    words_arr.each_with_index do |word, idx|
+      line += word + " "
+      if line.length >= 45 || idx == words_arr.length - 1
+        lines_arr << line.strip
+        if line.strip.length > max_line_length then max_line_length = line.strip.length end
+        line = ""
+      end
+    end
+    lines_arr.each do |line|
+      puts pad(max_line_length, line)
+    end
+  end
+  def pad(max, str)
+    pad = ""
+    padding_amount = ((max - str.length) / 2).ceil
+    padding_amount.times do |n|
+      pad += " "
+    end
+    centered = "#{pad}#{str}#{pad}"
   end
 end
