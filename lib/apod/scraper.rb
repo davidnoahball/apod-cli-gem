7be50808170d464b6f6080ce92e26f75d12ddf04
davidnoahball <davidnoahball@gmail.com>
2016-07-19 08:44:34 -0700

    Finished date creation in dates hash

--- lib/apod/scraper.rb
+++ lib/apod/scraper.rb
@@ -8,16 +8,21 @@ class Scraper
 
   def self.dates_hash
     array = []
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     content = self.get_page.css("body b")
-    date_titles = content.text.split("\n").reject!{|item| item == ""}
+    date_titles = content.text.split("\n").reject!{|item| item == "" || item == ""}
     date_titles.pop
-    date_titles.each do |dt|
+
+    date_titles.each_with_index do |dt, idx|
       hash = {}
-      hash[:original] = dt
-      hash[:date] = "#{dt.slice(0, 4)}-#{dt.match(/[0-9]{2}/, 5)}"
-      hash[:title] = nil
+      if idx == date_titles.length - 1 || months.index(date_titles[idx + 1].match(/[a-zA-Z]{1,}/).to_s).nil?
+        date_titles[idx] = "#{date_titles[idx]} #{date_titles[idx + 1]}"
+        date_titles.delete_at(idx + 1)
+      end
+      hash[:date] = "#{dt.match(/[0-9]{4}/)}-#{months.index(dt.match(/[a-zA-Z]{1,}/).to_s) + 1}-#{dt.match(/[0-9]{2}/)}"
       array << hash
     end
+
     array
   end
 end
