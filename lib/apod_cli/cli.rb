ed7dcc8a558e988bf97305d02677574e1ac8ff59
davidnoahball <davidnoahball@gmail.com>
2016-07-20 13:25:12 -0700

    Worked out basic CLI structure.

--- lib/apod_cli/cli.rb
+++ lib/apod_cli/cli.rb
@@ -6,16 +6,40 @@ require_relative "printer"
 require_relative "scraper"
 
 class CLI
-  def call
+  def call #All CLI logic should come from this command
+    @scraper = Scraper.new
+    @printer = Printer.new
+    @data = @scraper.data
+    puts "\nHello.\n"
     start
   end
+
   def start
-    scrape = Scraper.new
-    print = Printer.new
-    data = scrape.data
-    data.each_with_index do |hash, idx|
-      print.print_link(scrape.data[1000], "[1]".colorize(:red))
-      print.print_page(scrape.pic_data(hash[:link]))
+    puts "Which type of lookup would you like to perform?"
+    puts ""
+=begin
+    @data.each_with_index do |hash, idx|
+      @printer.print_link(@data[1000], "[#{idx + 1}] ")
+      @printer.print_page(@scraper.pic_data(hash[:link]))
+    end
+=end
+
+    puts "Would you like to perform another lookup?\n[y/n]"
+    if valid_input(["y", "n"]) == "y"
+      puts ""
+      start
+    else
+      puts "Goodbye."
     end
   end
+
+  def valid_input(wanted)
+  input = gets.chomp.downcase
+  if wanted.include?(input)
+    return input
+  else
+    puts "That's not a valid input! Please try again."
+    valid_input(wanted)
+  end
+end
 end
--- lib/apod_cli/printer.rb
+++ lib/apod_cli/printer.rb
@@ -7,7 +7,12 @@ require_relative "scraper"
 
 class Printer
   def print_link(link_hash, start="")
-    puts start + "The astronomy picture of the day on " + link_hash[:date].colorize(:green) + " was " + link_hash[:name].colorize(:green) + "."
+    lines = start.colorize(:red) + "The astronomy picture of the day on " + link_hash[:date].colorize(:green) + " was " + link_hash[:name].colorize(:green) + ".\n"
+    start.length.times do |n|
+      lines += " "
+    end
+    lines += "The link is " + link_hash[:link].colorize(:green) + "."
+    puts lines
   end
   def print_page(page_hash)
     words_arr = page_hash[:expl].split(" ")
@@ -16,7 +21,7 @@ class Printer
     max_line_length = 0
     words_arr.each_with_index do |word, idx|
       line += word + " "
-      if line.length >= 120 || idx == words_arr.length - 1
+      if line.length >= 100 || idx == words_arr.length - 1
         lines_arr << line.strip
         if line.strip.length > max_line_length then max_line_length = line.strip.length end
         line = ""
