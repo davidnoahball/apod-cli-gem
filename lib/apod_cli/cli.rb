7bacc2dd91169ae7c6740bcc9013cccf77420a9e
davidnoahball <davidnoahball@gmail.com>
2016-07-20 13:59:38 -0700

    Started search code

--- lib/apod_cli/cli.rb
+++ lib/apod_cli/cli.rb
@@ -7,23 +7,33 @@ require_relative "scraper"
 
 class CLI
   def call #All CLI logic should come from this command
+    puts "\nHello.\n"
     @scraper = Scraper.new
     @printer = Printer.new
     @data = @scraper.data
-    puts "\nHello.\n"
     start
   end
 
   def start
-    puts "Which type of lookup would you like to perform?"
-    puts ""
+    puts "Which type of APOD lookup would you like to perform?"
+    puts "[1]".colorize(:red) + " Search by date"
+    puts "[2]".colorize(:red) + " Search by name"
+    puts "[3]".colorize(:red) + " Sample data"
+    search_type = valid_input(["1", "2", "3"]).to_i
+    case search_type
+    when 1
+      search(:date)
+    when 2
+      search(:name)
+    when 3
+      sample
+    end
 =begin
     @data.each_with_index do |hash, idx|
       @printer.print_link(@data[1000], "[#{idx + 1}] ")
       @printer.print_page(@scraper.pic_data(hash[:link]))
     end
 =end
-
     puts "Would you like to perform another lookup?\n[y/n]"
     if valid_input(["y", "n"]) == "y"
       puts ""
@@ -33,8 +43,25 @@ class CLI
     end
   end
 
+  def sample
+
+  end
+
+  def search(type)
+    search_array = []
+    results = @data.select{|hash| search_array.include?(hash[type])}
+  end
+
+  def print_links(arr)
+
+  end
+
+  def print_pages(arr)
+
+  end
+
   def valid_input(wanted)
-  input = gets.chomp.downcase
+  input = gets.chomp.strip.downcase
   if wanted.include?(input)
     return input
   else
